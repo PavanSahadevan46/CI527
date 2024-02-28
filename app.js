08b2f9045c9f88fa335816e394b4168d7cb2683e
Pavan Sahadevan <93722550+PavanSahadevan46@users.noreply.github.com>
2024-02-28 10:24:50 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 window.addEventListener("load",function(evt){
-    const q7button = document.querySelector("#btn1");
-    q7button.addEventListener('click',function(evt){
-      //get input from search
-       var searchTerm = "egypt";    
-        //construct url
-       //https://images-api.nasa.gov/search?q=Apollo%2011&media_type=image
-       
 
-
-        // const url = 'https://images-api.nasa.gov/search?q=Apollo%2011&media_type=image&q=' + encodeURIComponent(search);
-        const url = 'https://api.vam.ac.uk/v2/objects/search?q=' + encodeURIComponent(searchTerm);
-
-        console.log(url);
-
-        var xhr = new XMLHttpRequest;
+    const searchForm = document.querySelector("#searchForm");
+    searchForm.addEventListener('submit',function(event){
+        event.preventDefault();
+        var userInput = document.querySelector('#query').value;
+        const url = 'https://api.vam.ac.uk/v2/objects/search?q='+ encodeURIComponent(userInput);
+        var xhr = new XMLHttpRequest();
         xhr.addEventListener("load",function(){
             if(xhr.status == 200){
                 console.log(xhr.responseText);
                 const results = document.querySelector("#results");
                 const data = JSON.parse(xhr.responseText);
+
+                while(results.firstChild){
+                    results.removeChild(results.lastChild);
+                }
+                
                 for(record of data.records){
                     const objectName = record._primaryTitle;
-                    console.log(objectName);
-                    const para = document.createElement('p');
-                    para.textContent = objectName;
-                    results.appendChild(para);
-                    const img = document.createElement('img');
+                    const objectDate = record._primaryDate;
+                    // console.log(objectName);
+                    // const para = document.createElement('p');
+                    // para.textContent = objectName + "" + objectDate;
+                    // results.appendChild(para);
+                    const img = document.querySelector('.card img');
+                    const title = document.querySelector('.card.container h4');
                     img.setAttribute('src',record._images._primary_thumbnail);
-                    results.appendChild(img);
-
-
-
-
+                    // title.textContent = objectName;
+                    // results.appendChild(img);
                 }
+            
             }else{
                 console.log('Status code: ' + xhr.status);
             }
@@ -42,14 +39,7 @@ window.addEventListener("load",function(evt){
         xhr.send();
 
     })
-
-    const searchForm = document.querySelector("#searchForm");
-    searchForm.addEventListener('submit',function(event){
-        event.preventDefault();
-        var userInput = document.querySelector('#query').value;
-        console.log(userInput);
-
-    });
+});
 
 
 
@@ -59,23 +49,20 @@ window.addEventListener("load",function(evt){
 
 
 
-})
+
+
+
+
+
+
+
+
+
 
 
 // img.setAttribute('src',item.links[0].href );
 // img.setAttribute('alt',item.data[0].description);
 // results.appendChild(img);
-
-
-
-
-
-
-
-
-
-
-
 // console.log(item.data[0].description);
 // console.log(item.links[0].href);
 
