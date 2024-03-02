34d107f275a1d08f2654b8df0e36eeca30522497
PavanSahadevan46 <pavansahadevan46@gmail.com>
2024-03-02 15:23:40 +0000

    started development on css,created a todo,added a loading gif before search results appear.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener("load",function(evt){
 
     const searchForm = document.querySelector("#searchForm");
     const container = document.querySelector('#container1');
-
+    const loadingIcon = document.querySelector(".lds-dual-ring");
 
     searchForm.addEventListener('submit',function(event){
         event.preventDefault();
@@ -10,6 +10,7 @@ window.addEventListener("load",function(evt){
         const url = 'https://api.vam.ac.uk/v2/objects/search?q='+ encodeURIComponent(userInput);
         var xhr = new XMLHttpRequest();
         xhr.addEventListener("load",function(){
+            loadingIcon.classList.add("hidden");
             if(xhr.status == 200){
                 console.log(xhr.responseText);
                 const data = JSON.parse(xhr.responseText);
@@ -29,27 +30,27 @@ window.addEventListener("load",function(evt){
                     if (record._images._iiif_image_base_url == null){
                         imageElem.setAttribute('src',"imageNotFound.png");
                     }else{
-                        imageElem.setAttribute('src',record._images._iiif_image_base_url + "full/full/0/default.jpg"); 
+                        imageElem.setAttribute('src',record._images._iiif_image_base_url + "full/700,700/0/default.jpg"); 
                     }
                     // imageElem.setAttribute('alt', )   look where alt is in json
                     imageElem.classList.add('objImage');
                     cardDiv.appendChild(imageElem);
                     
                     const titleDiv = document.createElement('div');
-                    titleDiv.classList.add('objTitle');
                     const titleHeading = document.createElement('h4');
+                    titleDiv.classList.add('objTitle');
                     titleHeading.textContent = objectName;
                     titleDiv.appendChild(titleHeading);
                     cardDiv.appendChild(titleDiv);
 
                     const dateDiv = document.createElement('div');
-                    dateDiv.classList.add('objDate');
                     const dateHeading = document.createElement('h3');
+                    dateDiv.classList.add('objDate');
                     dateHeading.textContent = objectDate;
                     dateDiv.appendChild(dateHeading);
                     cardDiv.appendChild(dateDiv);
 
-                    container1.appendChild(cardDiv);
+                    container.appendChild(cardDiv);
                 }
             
             }else{
@@ -59,6 +60,8 @@ window.addEventListener("load",function(evt){
 
         xhr.open("GET",url,true);
         xhr.send();
+        loadingIcon.classList.remove("hidden");
+        
 
     })
 });
