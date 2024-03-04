c497cf65ae73196a7c58017f84edb191e75100b8
PavanSahadevan46 <pavansahadevan46@gmail.com>
2024-03-04 19:29:25 +0000

    added basic input validation in form of div and paragraph

--- app.js
+++ app.js
@@ -1,69 +1,79 @@
-window.addEventListener("load",function(evt){
+window.addEventListener("load", function (evt) {
 
     const searchForm = document.querySelector("#searchForm");
     const container = document.querySelector('#container1');
     const loadingIcon = document.querySelector(".lds-dual-ring");
+    const inputValidation = document.querySelector('.inputValidation');
 
-    searchForm.addEventListener('submit',function(event){
+
+    inputValidation.classList.add("hidden");
+    searchForm.addEventListener('submit', function (event) {
         event.preventDefault();
         var userInput = document.querySelector('#query').value;
-        const url = 'https://api.vam.ac.uk/v2/objects/search?q='+ encodeURIComponent(userInput);
+        const url = 'https://api.vam.ac.uk/v2/objects/search?q=' + encodeURIComponent(userInput);
         var xhr = new XMLHttpRequest();
-        xhr.addEventListener("load",function(){
-            loadingIcon.classList.add("hidden");
-            if(xhr.status == 200){
-                console.log(xhr.responseText);
-                const data = JSON.parse(xhr.responseText);
+        if (userInput == "") {
+            inputValidation.classList.remove("hidden");
+        } else {
+            xhr.addEventListener("load", function () {
+                loadingIcon.classList.add("hidden");
+                inputValidation.classList.add("hidden");
+                if (xhr.status == 200) {
+                    console.log(xhr.responseText);
+                    const data = JSON.parse(xhr.responseText);
 
-                while(container.firstChild){
-                    container.removeChild(container.lastChild);
-                }
-                
-                for(record of data.records){
-                    const objectName = record._primaryTitle;
-                    const objectDate = record._primaryDate;
-                    
-                    const cardDiv = document.createElement('div');
-                    cardDiv.classList.add('card');
-                    
-                    const imageElem= document.createElement('img');
-                    if (record._images._iiif_image_base_url == null){
-                        imageElem.setAttribute('src',"imageNotFound.png");
-                    }else{
-                        imageElem.setAttribute('src',record._images._iiif_image_base_url + "full/700,700/0/default.jpg"); 
+                    while (container.firstChild) {
+                        container.removeChild(container.lastChild);
                     }
-                    // imageElem.setAttribute('alt', )   look where alt is in json
-                    imageElem.classList.add('objImage');
-                    cardDiv.appendChild(imageElem);
-                    
-                    const titleDiv = document.createElement('div');
-                    const titleHeading = document.createElement('h4');
-                    titleDiv.classList.add('objTitle');
-                    titleHeading.textContent = objectName;
-                    titleDiv.appendChild(titleHeading);
-                    cardDiv.appendChild(titleDiv);
 
-                    const dateDiv = document.createElement('div');
-                    const dateHeading = document.createElement('h3');
-                    dateDiv.classList.add('objDate');
-                    dateHeading.textContent = objectDate;
-                    dateDiv.appendChild(dateHeading);
-                    cardDiv.appendChild(dateDiv);
+                    for (record of data.records) {
+                        const objectName = record._primaryTitle;
+                        const objectDate = record._primaryDate;
 
-                    container.appendChild(cardDiv);
+                        const cardDiv = document.createElement('div');
+                        cardDiv.classList.add('card');
+
+                        const imageElem = document.createElement('img');
+                        if (record._images._iiif_image_base_url == null) {
+                            imageElem.setAttribute('src', "imageNotFound.png");
+                        } else {
+                            imageElem.setAttribute('src', record._images._iiif_image_base_url + "full/700,700/0/default.jpg");
+                        }
+                        // imageElem.setAttribute('alt', )   look where alt is in json
+                        imageElem.classList.add('objImage');
+                        cardDiv.appendChild(imageElem);
+
+                        const titleDiv = document.createElement('div');
+                        const titleHeading = document.createElement('h4');
+                        titleDiv.classList.add('objTitle');
+                        titleHeading.textContent = objectName;
+                        titleDiv.appendChild(titleHeading);
+                        cardDiv.appendChild(titleDiv);
+
+                        const dateDiv = document.createElement('div');
+                        const dateHeading = document.createElement('h3');
+                        dateDiv.classList.add('objDate');
+                        dateHeading.textContent = objectDate;
+                        dateDiv.appendChild(dateHeading);
+                        cardDiv.appendChild(dateDiv);
+
+                        container.appendChild(cardDiv);
+                    }
+
+                } else {
+                    console.log('Status code: ' + xhr.status);
                 }
-            
-            }else{
-                console.log('Status code: ' + xhr.status);
-            }
-        })
+            })
 
-        xhr.open("GET",url,true);
-        xhr.send();
-        loadingIcon.classList.remove("hidden");
-        
+            xhr.open("GET", url, true);
+            xhr.send();
+            loadingIcon.classList.remove("hidden");
 
-    })
+
+
+        }
+
+    });
 });
 
 
