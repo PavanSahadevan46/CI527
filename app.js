c7067bb58c80a09579c46f2cbaea625986a0db51
Pavan Sahadevan (student) <ps750@uni.brighton.ac.uk>
2024-03-06 10:27:31 +0000

    changed spinner, started development on object validation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 window.addEventListener("load", (evt) => {
 
-    const searchForm = document.querySelector(".searchForm");
+    const searchForm = document.querySelector('.searchForm');
     const container = document.querySelector('.container1');
-    const loadingIcon = document.querySelector(".lds-dual-ring");
+    const loadingIcon = document.querySelector('.lds-dual-ring');
+    const loadingCircle = document.querySelector('.loader');
     const inputValidation = document.querySelector('.inputValidation');
+    const objectValidation = document.querySelector('.objectValidation');
     inputValidation.classList.add("hidden");
-
+    objectValidation.classList.add("hidden");
+    
 
 
     searchForm.addEventListener('submit', async (event) => {
@@ -15,7 +18,7 @@ window.addEventListener("load", (evt) => {
         if (userInput == "") {
             inputValidation.classList.remove("hidden");
         } else {
-            loadingIcon.classList.remove("hidden");
+            loadingCircle.classList.remove("hidden");
             inputValidation.classList.add("hidden");
             try {
                 const response = await fetch(url);
@@ -36,20 +39,21 @@ window.addEventListener("load", (evt) => {
                 for (record of data.records) {
                     const objectName = record._primaryTitle;
                     const objectDate = record._primaryDate;
+                    const objectMaker = record._primaryMaker.name;
 
                     const cardDiv = document.createElement('div');
                     cardDiv.classList.add('card');
 
                     const imageElem = document.createElement('img');
                     if (record._images._iiif_image_base_url == null) {
-                        imageElem.setAttribute('src', "imageNotFound.png");
+                        imageElem.setAttribute('src', "unavailableImage.png");
                     } else {
                         imageElem.setAttribute('src', record._images._iiif_image_base_url + "full/full/0/default.jpg");
                     }
-                    // imageElem.setAttribute('alt', )   look where alt is in json
+                    imageElem.setAttribute('alt', "This is " + objectName + ". " + "by " + objectMaker);
                     imageElem.classList.add('objImage');
                     cardDiv.appendChild(imageElem);
-
+                    
                     const titleDiv = document.createElement('div');
                     const titleHeading = document.createElement('h4');
                     titleDiv.classList.add('objTitle');
@@ -66,12 +70,18 @@ window.addEventListener("load", (evt) => {
                     dateDiv.appendChild(dateHeading);
                     cardDiv.appendChild(dateDiv);
 
+                    const nameDiv = document.createElement('div');
+
+
+
+
+
                     container.appendChild(cardDiv);
                 }
             } catch (error) {
                 console.log(error);
             } finally {
-                loadingIcon.classList.add("hidden");
+                loadingCircle.classList.add("hidden");
             }
         }
 
